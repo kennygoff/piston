e13031f3c709a0e47ac1a40fd9bb296ddcd786a2
Kenny Goff <goff.kenneth@gmail.com>
2014-07-01 01:11:24 -0400

    Simple one-to-one build directory mapping and Jinja templating

---
+++ README.md
@@ -0,0 +1,33 @@
+# piston
+
+A simple static site generator (currently supporting Jinja2 templates)
+
+*This is a work in progress, feel free to try it out, but it is currently being
+built and actively modified, and expanded on.*
+
+### Setup
+
+Install [Jinja2](http://jinja.pocoo.org/)
+
+I use pip: `pip intall jinja2`
+
+More installation options are available in Jinja's docs.
+
+### Usage
+
+Build a site directory, and use Jinja2 HTML templates with the extension
+`.jinja`.
+
+Simply run the `piston.py` file inside the directory, and piston will create
+a `build` directory, with your entire main directory copied, and `.jinja` files
+built into `.html` files.
+
+### Advanced
+
+Files starting with `.` will be ignored by piston. This allows you to create
+base templates that you can furthervextend, but not have show up with its own
+`.html` file.
+
+For example, I use `.base.jinja` and extend `index.jinja` from the base. Piston
+will simply ignore this file and only generate `index.html` in `build`
+directory.
---
+++ piston.py
@@ -0,0 +1,37 @@
+import jinja2
+import os
+import shutil
+
+template_loader = jinja2.FileSystemLoader(searchpath=os.getcwd())
+template_env = jinja2.Environment(loader=template_loader)
+
+if not os.path.exists("./build"):
+    os.makedirs("./build")
+
+for root, dirs, files in os.walk("."):
+    if len(filter(root.startswith, ["./.git", "./build", "./venv"])):
+        continue
+
+    build_root = root[:1] + "/build" + root[1:]
+    if not os.path.exists(build_root):
+        os.makedirs(build_root)
+
+    for filebase in files:
+        if filebase.startswith("."):
+            continue
+
+        filename, fileext = os.path.splitext(filebase)
+        fileroot = "%s/%s" % (root, filebase)
+        filebuild = "%s/%s" % (build_root, filebase)
+
+        if fileext == ".jinja":
+            template = template_env.get_template(fileroot)
+            template_vars = {
+                "relroot": "../" * (build_root.count("/") - 1)
+            }
+            output = template.render(template_vars)
+
+            with open(filebuild, "wb") as fh:
+                fh.write(output)
+        else:
+            shutil.copy(fileroot, filebuild)
